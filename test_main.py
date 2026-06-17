69f7608c5ed21bf02482ccce2167edeedb48cdcd
Surajsase <surajsase2125@gmail.com>
2026-06-17 23:49:14 +0530

    Initial commit

---
+++ test_main.py
@@ -0,0 +1,14 @@
+import sys
+from io import StringIO
+import unittest
+from unittest.mock import patch
+from main import hello
+
+class TestHelloFunction(unittest.TestCase):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_hello(self, mock_stdout):
+        hello()
+        self.assertEqual(mock_stdout.getvalue(), 'hi\n')
+
+if __name__ == '__main__':
+    unittest.main()
